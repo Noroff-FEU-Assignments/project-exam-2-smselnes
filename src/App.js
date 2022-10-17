e3f219f4314d6fa55596d4d0c8f6243c44946ed2
smselnes <smselnes@gmail.com>
2022-10-17 21:42:18 +0200

    Initial
    Created React app.
    Created an assets folder.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Project Exam 2</h1>
+    </div>
+  );
+}
+
+export default App;
