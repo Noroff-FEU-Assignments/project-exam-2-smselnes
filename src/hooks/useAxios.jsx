4563abb947510bd299a90380e0a47ce7cb2090ad
smselnes <smselnes@gmail.com>
2022-11-09 09:50:59 +0100

    API updates

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,19 +1,20 @@
 import { useContext } from "react";
 import axios from "axios";
 import AuthContext from "../context/AuthContext";
-import { base_url } from "../utils/Api";
-
-const url = base_url;
+import { social_url } from "../utils/Api";
+//import { base_url } from "../utils/Api";
 
 export default function useAxios() {
   const [auth] = useContext(AuthContext);
-
   const apiClient = axios.create({
-    baseURL: url,
+    baseURL: social_url,
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 
   apiClient.interceptors.request.use(function (config) {
-    const token = auth.token;
+    const token = auth.accessToken;
     config.headers.Authorization = token ? `Bearer ${token}` : "";
     return config;
   });
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,15 +1,7 @@
-//base url for all endpoints
 export const base_url = "https://nf-api.onrender.com/";
 
-//base url for all social endpoints
 export const social_url = "https://nf-api.onrender.com/api/v1/social/";
 
-//auth endpoint for registering users
 export const register_path = "api/v1/social/auth/register";
 
-//login path
 export const login_path = "api/v1/social/auth/login";
-
-//personal user token
-export const userToken =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTcyOSwibmFtZSI6InN0YWFsZV9zZWxuZXMiLCJlbWFpbCI6IlN0YVNlbDg3MDYwQHN0dWQubm9yb2ZmLm5vIiwiYXZhdGFyIjpudWxsLCJiYW5uZXIiOm51bGwsImlhdCI6MTY2NjA3OTcyMn0.KWUJGgX-o4DfKtph3uSSdT-AMYgil9U-nBzrua5Qiek";
