08908810e546f72170a4fac5e9397cbb0db780fe
smselnes <smselnes@gmail.com>
2022-11-11 10:33:52 +0100

    Delete post functionality

--- src/components/dashboard/DeletePost.jsx
+++ src/components/dashboard/DeletePost.jsx
@@ -1,15 +1,24 @@
 import useAxios from "../../hooks/useAxios";
 import { useState } from "react";
-import { Alert } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function DeletePost({ id }) {
   const http = useAxios();
   const [show, setShow] = useState(false);
 
+  /* function successfullyDeleted() {
+    setTimeout(() => {
+      window.location.reload(true);
+    }, 5000);
+  } */
+
   async function deleteOwnPost() {
     try {
       const response = await http.delete(`posts/${id}`);
       console.log(response);
+
+      window.location.reload(true);
     } catch (error) {
       console.log(error.toString());
     }
@@ -17,13 +26,20 @@ export default function DeletePost({ id }) {
 
   return (
     <>
-      <Alert variant="error" show={show}>
+      <Alert className="ownPost__delete__alert" show={show}>
         <Alert.Heading>Confirm delete post?</Alert.Heading>
+        <Button onClick={deleteOwnPost}>Yes</Button>
+        <Button
+          onClick={() => {
+            setShow(false);
+          }}
+        >
+          No
+        </Button>
       </Alert>
       <button
-        className="btn btn-danger"
+        className="btn btn-danger "
         onClick={() => {
-          deleteOwnPost();
           setShow(true);
         }}
       >
